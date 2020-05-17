f0c22658c24f80dbe18d30657da625195dfc2d4d
Derhks <derhks@gmail.com>
2020-05-17 20:10:35 +0000

    [Update] sort the list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,10 +4,10 @@ window.onload = () => {
     function(){
         if ($(this).is(':checked')) {
           checkAmenity[this.dataset.id] = this.dataset.name;
-          $('div.amenities h4').text(Object.values(checkAmenity));
+          $('div.amenities h4').text(Object.values(checkAmenity).sort());
         } else {
           delete checkAmenity[this.dataset.id];
-          $('div.amenities h4').text(Object.values(checkAmenity));
+          $('div.amenities h4').text(Object.values(checkAmenity).sort());
         }
     });
 };
