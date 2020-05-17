79c15ccc3d0c7eeff2414e9f9c5dafdb7466e8ef
Derhks <derhks@gmail.com>
2020-05-17 21:41:40 +0000

    [Feature] sort and join

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,10 +4,10 @@ window.onload = () => {
     function(){
         if ($(this).is(':checked')) {
           checkAmenity[this.dataset.id] = this.dataset.name;
-          $('div.amenities h4').text(Object.values(checkAmenity).sort());
+          $('div.amenities h4').text(Object.values(checkAmenity).sort().join(', '));
         } else {
           delete checkAmenity[this.dataset.id];
-          $('div.amenities h4').text(Object.values(checkAmenity).sort());
+          $('div.amenities h4').text(Object.values(checkAmenity).sort().join(', '));
         }
     });
 };
